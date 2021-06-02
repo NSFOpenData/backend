176307cac419d3fc6762a3bc3b236ccd371d9cea
Aadi Bajpai <hello@aadibajpai.com>
2021-06-02 14:43:25 -0500

    add enum validation in db as well

--- models/user.js
+++ models/user.js
@@ -7,7 +7,11 @@ const userSchema = new Schema(
         name: { type: String, required: true },
         email: { type: String, required: true },
         password: { type: String, required: true },
-        role: { type: String, required: true },
+        role: { 
+            type: String,
+            enum: ["USER", "ADMIN", "PRIVILEGED"],
+            required: true 
+        },
     },
     {
         timestamps: true,
