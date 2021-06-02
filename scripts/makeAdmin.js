d1528fe0319d9502171b1508228f0e5496384901
Aadi Bajpai <hello@aadibajpai.com>
2021-06-02 12:17:11 -0500

    script works perfectly

--- scripts/makeAdmin.js
+++ scripts/makeAdmin.js
@@ -3,29 +3,28 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 function printUsage() {
-    console.log("hello");
+    console.log("Usage: node scripts/makeAdmin.js -- name@email.com,another@email.com");
+    process.exit(0);
 }
 
 async function changeRole(email, role) {
     console.log(email);
-    let user = await User.find({email: email});
+    let user = await User.findOne({ email: email });
     if (!user) throw new Error(`User with email ${email} not found.`);
-    console.log(user);
+    user.role = role;
+    await user.save();
+    console.log(`User ${email} is now an admin :)`);
 }
 
-
 (async () => {
-    const args = process.argv.splice(process.execArgv.length);
-    console.log(args);
-    if (!args.length || args.length > 4) printUsage();
+    const args = process.argv;
+    if (!args.length || args.length !== 4) printUsage();
 
     try {
         const uri = process.env.DB;
         const options = { useNewUrlParser: true, useUnifiedTopology: true };
         await mongoose.connect(uri, options);
-        await Promise.all(
-            args[3].split(',').map(email => changeRole(email, "ADMIN"))
-        );
+        await Promise.all(args[3].split(",").map(email => changeRole(email, "ADMIN")));
         process.exit(0);
     } catch (e) {
         console.error(e);
