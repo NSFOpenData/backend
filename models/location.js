f7e0dfc31790b692ed1834ca0a1edd276a9d087e
Aadi Bajpai <hello@aadibajpai.com>
2021-07-05 15:04:33 -0500

    format

--- models/location.js
+++ models/location.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
-const locationSchema = new Schema(
-    {
-        lat: { type: String, required: true },
-        lon: { type: String, required: true },
-        name: String,
-    },
-);
+const locationSchema = new Schema({
+    lat: { type: String, required: true },
+    lon: { type: String, required: true },
+    name: String,
+});
 
 module.exports = { locationSchema };
