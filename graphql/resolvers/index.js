91da81be74d95b460b0dded914f941e5ca461553
Aadi Bajpai <hello@aadibajpai.com>
2021-07-20 12:04:12 -0700

    add awaits

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -87,7 +87,7 @@ module.exports = {
         const { neighborhood, role } = user[DOMAIN];
         if (role === "USER") throw new Error("Need to be at least a privileged user.");
         const partialVehicleDoc = new PartialVehicle({ createdBy: user.sub, neighborhood, ...partial });
-        const partialVehicle = partialVehicleDoc.save().then(a => a.populate("createdBy").execPopulate());
+        const partialVehicle = await partialVehicleDoc.save().then(a => a.populate("createdBy").execPopulate());
         console.log("created partial vehicle description", partialVehicle);
         return partialVehicle;
     },
@@ -123,7 +123,7 @@ module.exports = {
         if (!user) throw new Error("Authentication needed");
         const { neighborhood } = user[DOMAIN];
         const partialAnimalDoc = new PartialAnimal({ createdBy: user.sub, neighborhood, ...partial });
-        const partialAnimal = partialAnimalDoc.save().then(a => a.populate("createdBy").execPopulate());
+        const partialAnimal = await partialAnimalDoc.save().then(a => a.populate("createdBy").execPopulate());
         console.log("created partial animal description", partialAnimal);
         return partialAnimal;
     },
