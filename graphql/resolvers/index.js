d88de79c2a534d51ba308ca57baa22d52e38a59c
Kevin Jin <kevjin2002@gmail.com>
2022-04-11 09:38:27 -0500

    Updated helmet

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -7,14 +7,14 @@ const { Vehicle, PartialVehicle } = require("../../models/vehicle");
 const { Animal, PartialAnimal } = require("../../models/animal");
 const User = require("../../models/user");
 const Neighborhood = require("../../models/neighborhood");
-// var admin = require("firebase-admin");
-import { GoogleAuth } from "@codetrix-studio/capacitor-google-auth";
+var admin = require("firebase-admin");
+// import { GoogleAuth } from "@codetrix-studio/capacitor-google-auth";
 
 var uuid = require("uuid");
 
 const DOMAIN = "https://nsf-scc1.isis.vanderbilt.edu/graphql";
 
-GoogleAuth.init({
+admin.initializeApp({
     apiKey: "AIzaSyBdtgJTpg8-pYIb7sMny70qeJICM-fiSqY",
     authDomain: "nsfopendata.firebaseapp.com",
     projectId: "nsfopendata",
@@ -219,7 +219,8 @@ module.exports = {
             // idToken comes from the client app
             var token = " ";
 
-            GoogleAuth.signIn()
+            admin
+                .auth()
                 .verifyIdToken(idToken)
                 .then(decodedToken => {
                     const uid = decodedToken.uid;
